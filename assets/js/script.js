c4ac630fb1050fc8d5f67363534f31af73228bcf
swonnie <swonnie@gmail.com>
2024-02-25 14:56:51 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* 
 $(function () { // wait for document ready
     // init controller
     var controller1 = new ScrollMagic.Controller({container: "#right"});
@@ -13,3 +14,29 @@ $(function () { // wait for document ready
         .setClassToggle("#reveal2", "visible")
         .addIndicators() // add indicators (requires plugin)
 });
+*/
+
+const images = document.querySelectorAll( '.imagebox > div' );
+const imageAnchors = document.querySelectorAll( '.image-anchor' );
+
+const contentColumn = document.getElementById( 'center' );
+
+const toggleVisibleImages = event => {
+    const screenCenter = contentColumn.offsetHeight / 2;
+
+    let activeAnchorLink = -1;
+    imageAnchors.forEach( imageAnchor => {
+        const imageTop = imageAnchor.offsetTop - contentColumn.scrollTop;
+
+        if ( imageTop <= screenCenter )
+            activeAnchorLink = imageAnchor.dataset.imageId;
+    } );
+
+    images.forEach( image => {
+        const imageId = image.id;
+
+        const showImage = ( imageId == activeAnchorLink );
+        image.classList.toggle( 'visible', showImage );
+    } );
+}
+contentColumn.addEventListener( 'scroll', toggleVisibleImages );
